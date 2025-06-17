be31efb684b6d76c816843036131e1132e6c9a48
Sammy-Mbugua <msammy542@gmail.com>
2025-06-17 06:20:50 +0300

    feat:get_bet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES=3
+MAX_BET=100
+MIN_BET=1
 
 
 
@@ -20,21 +22,39 @@ def deposit():
 
 def get_number_of_lines():
     while True:
-        lines= input("Enter the number of lines(1-" + str{MAX_LINES} ")? $")
+        lines= input(
+            "Enter the number of lines(1-" + str{MAX_LINES} + ")? $")
         if lines.isdigit():
             lines=int(lines)
             if 1 <= lines <= MAX_LINES:
                 break
             else:
-                print(f"The value must be between {MAX_LINES} than 0.")
+                print("The value must be between {MAX_LINES} than 0.")
         else:
             print("Enter the number")
 
     return lines
 
+def get_bet():
+    while True:
+        amount= input("Enter the amount you want to bet on each line? $")
+        if amount.isdigit():
+            amount=int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"The value must between {MIN_BET} - {MAX_BET}.")
+        else:
+            print("Enter the number")
+
+    return amount
+
 
 def main():
-    balance=deposit
-    lines=get_number_of_lines
+    balance=deposit()
+    lines=get_number_of_lines()
+    bet=get_bet()
+    total_bet=bet*lines
+    print("")
             
             
