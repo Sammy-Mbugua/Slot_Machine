2f413549fb42cff47afee1b9f30fd2a76039be14
Sammy-Mbugua <msammy542@gmail.com>
2025-06-18 02:37:58 +0300

    feat:random choice,removing and appending the value

--- main.py
+++ main.py
@@ -21,6 +21,21 @@ def get_slot_machine_spin(rows,cols,symbols):
             all_symbols.append(symbol)
 
 
+    columns=[]
+    for _ in range(cols):
+        column=[]
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value=random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+    columns.append(column)
+    return columns
+
+
+
+
 
 def deposit():
     while True:
