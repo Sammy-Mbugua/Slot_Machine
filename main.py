5cb396752b4ce20d49fc2659a29b0ff4d45a0ae3
Sammy-Mbugua <msammy542@gmail.com>
2025-06-18 03:22:50 +0300

    feat:check winnings

--- main.py
+++ main.py
@@ -14,6 +14,26 @@ symbol_count= {
     "D":8,
 
 }
+symbol_value= {
+    "A":5,
+    "B":4,
+    "C":3,
+    "D":2,
+
+}
+
+def check_winnings(columns,bet,lines,values):
+    winnings=0
+    winning_lines= []
+    for line in range (lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+    return winnings,winning_lines
 def get_slot_machine_spin(rows,cols,symbols):
     all_symbols=[]
     for symbol,symbol_count in symbols.item():
@@ -30,12 +50,12 @@ def get_slot_machine_spin(rows,cols,symbols):
             current_symbols.remove(value)
             column.append(value)
 
-    columns.append(column)
+        columns.append(column)
     return columns
 
 
 
-
+ 
 
 def deposit():
     while True:
